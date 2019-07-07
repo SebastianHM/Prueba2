c72a0e5025c35396572240bce49ba3084579f75d
SebastianHM <52508338+SebastianHM@users.noreply.github.com>
2019-07-06 23:59:51 -0500

    apps
    carpeta files

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,47 @@
+class FiguresController < ApplicationController
+	
+	def index
+		@figures = Figure.all
+	end
+
+	def new
+		@figure = Figure.new	
+	end
+
+	def create
+		@figure = Figure.new(figure_params)
+		if @figure.save
+			redirect_to '/figures'
+		else
+			render '/figure/new'
+		end
+	end
+
+	def edit
+		@figure = Figure.find(params[:id])
+	end
+
+	def update
+		@figure = Figure.find(params[:id])
+		if @figure.update(figure_params)
+			redirect_to '/figures'
+		else
+			render '/figures/:id/edit'
+		end
+	end
+	
+	def destroy
+		@figure = Figure.find(params[:id])
+		@figure.destroy
+		redirect_to '/figures'
+	end
+
+
+
+	private
+		def figure_params
+			params.require(:figure).permit(:name,:description)
+		end
+
+
+end
---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,35 @@
+class PicturesController < ApplicationController
+
+	def index
+		@pictures = Picture.all
+	end
+	
+	def new
+		@picture = Picture.new
+		@figure = Figure.all
+		options=[]
+		cont=1
+		@figure.each do |f| 
+			options[cont]=[@figure.name,@figure.name]
+			cont =cont+1
+		end
+		@opts=options
+	end
+	
+	def create
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			redirect_to'/pictures'
+		else
+			render '/picture/new'
+		end
+	end
+	
+	private
+
+		def picture_params
+			params.require(:picture).permit(:name,:kind_selection,:sides,:description,:color_selection)
+		end
+
+
+end
