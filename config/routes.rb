06bb3c92dad143564da6cf605f3ca0f0dec6b7a1
SebastianHM <52508338+SebastianHM@users.noreply.github.com>
2019-07-07 00:01:38 -0500

    config
    carpeta config

---
+++ config/routes.rb
@@ -0,0 +1,49 @@
+Rails.application.routes.draw do
+  
+  
+	resources :figures
+  
+
+
+	
+	# For details on the DSL available within this file, 
+	
+	
+#see http://guides.rubyonrails.org/routing.html
+
+	
+	
+	get '/figures' => 'figures#index'
+	
+	
+	get '/figure/new' => 'figures#new'
+	
+	
+	post '/figures' => 'figures#create'
+	
+	
+	get '/figure/:id' => 'figures#show', as: 'fig'
+	
+	
+	get '/figures/:id/edit' => 'figures#edit', as: 'editfigure'
+	
+	
+	patch '/figure/:id' => 'figures#update'
+	
+	
+	put '/figure/:id' => 'figures#update'
+	
+	
+	delete '/figure/:id' => 'figures#destroy'
+
+	
+	get '/pictures' => 'pictures#index'
+	get '/picture/new' => 'pictures#new'
+	post '/pictures' => 'pictures#create'
+
+
+
+
+
+
+end
